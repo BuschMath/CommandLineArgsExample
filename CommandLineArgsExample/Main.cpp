111500874d6d978b058d4ec3e82d0f3f0e92a04a
Busch <busch@iccc.cc.ia.us>
2021-06-08 10:33:01 -0500

    Add project files.

---
+++ CommandLineArgsExample/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	float frstNumb = atof(argv[1]);
+	float scndNumb = atof(argv[2]);
+
+	cout << frstNumb + scndNumb;
+}
